43946723eba77d4ce3511b37ace98024522750aa
kagan-kervan <ahmetkagankervan@gmail.com>
2025-05-31 11:04:54 +0300

    Updated intent analysis

--- nlu.py
+++ nlu.py
@@ -1,10 +1,5 @@
+import joblib
+model = joblib.load("intent_classifier.pkl")
+
 def detect_intent(question: str) -> str:
-    q = question.lower()
-    if "nedir" in q or "tanımı" in q:
-        return "definition"
-    elif "çöz" in q or "nasıl yapılır" in q:
-        return "solution"
-    elif "örnek" in q:
-        return "example"
-    else:
-        return "explanation"
+    return model.predict([question])[0]
